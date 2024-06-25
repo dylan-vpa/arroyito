675087aa0cb942b5cf465672527cec50a6c4bae4
Dylan Alexander <dylanp.arango12@gmail.com>
2024-06-24 21:43:40 -0500

    Change: API Logic

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,12 +1,12 @@
+// Initialize latestData outside the handler function
+let latestData = { distance: null, waterDetected: null }; // Default to null
+
 const dataHandler = async (req, res) => {
   const allowedMethods = ['POST', 'GET'];
 
   if (!allowedMethods.includes(req.method)) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-
-  // Initialize or retrieve existing latestData outside the handler function
-  let latestData = global.latestData || { distance: null, waterDetected: null }; // Default to null
 
   if (req.method === 'POST') {
     const { distance, waterDetected } = req.body;
@@ -17,15 +17,15 @@ const dataHandler = async (req, res) => {
 
     console.log(`Received data: distance=${distance}cm, waterDetected=${waterDetected}`);
 
-    // Update global latestData
-    global.latestData = { distance, waterDetected };
+    // Update the global latestData
+    latestData = { distance, waterDetected };
 
     return res.status(200).json({ message: 'Datos recibidos correctamente' });
   } else if (req.method === 'GET') {
     if (latestData.distance !== null && latestData.waterDetected !== null) {
-      return res.status(200).json(latestData); 
+      return res.status(200).json(latestData);
     } else {
-      return res.status(204).json({ message: 'No data available yet' }); 
+      return res.status(204).json({ message: 'No data available yet' });
     }
   }
 };
