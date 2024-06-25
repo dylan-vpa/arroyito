79c7f97b6fd0daf12b1bc0def2267acf686391b1
Dylan Alexander <dylanp.arango12@gmail.com>
2024-06-24 21:21:26 -0500

    try #17

--- src/api/data.js
+++ src/api/data.js
@@ -1,30 +1,31 @@
 const dataHandler = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Permite solicitudes desde cualquier origen (ajusta según tus necesidades)
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  const allowedMethods = ['POST', 'GET'];
+
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
   // Initialize or retrieve existing latestData outside the handler function
   let latestData = global.latestData || { distance: null, waterDetected: null }; // Default to null
 
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const { distance, waterDetected } = req.body;
 
-    if (typeof distance !== "number" || typeof waterDetected !== "boolean") {
-      return res.status(400).json({ error: "Invalid data type" });
+    if (typeof distance !== 'number' || typeof waterDetected !== 'boolean') {
+      return res.status(400).json({ error: 'Invalid data type' });
     }
 
-    console.log(
-      `Received data: distance=${distance}cm, waterDetected=${waterDetected}`
-    );
+    console.log(`Received data: distance=${distance}cm, waterDetected=${waterDetected}`);
 
     // Update global latestData
     global.latestData = { distance, waterDetected };
 
-    return res.status(200).json({ message: "Datos recibidos correctamente" });
-  } else if (req.method === "GET") {
+    return res.status(200).json({ message: 'Datos recibidos correctamente' });
+  } else if (req.method === 'GET') {
     if (latestData.distance !== null && latestData.waterDetected !== null) {
-      return res.status(200).json(latestData);
+      return res.status(200).json(latestData); 
     } else {
-      return res.status(204).json({ message: "No data available yet" });
+      return res.status(204).json({ message: 'No data available yet' }); 
     }
   }
 };
